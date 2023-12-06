d9db5a5bc6a7fda2abb5fca0bccb806a6ac04c6b
soumyadipghorai <ghorai.soumyadip33@gmail.com>
2023-12-07 00:29:51 +0530

    fetch and async added

--- week5/screencast/async_await.js
+++ week5/screencast/async_await.js
@@ -38,3 +38,65 @@ wish = await say_hello1()
 console.log(wish); 
 console.log("at the end");
 
+async function say_hello1(){
+    return new Promise((resolve, reject) => {
+        setTimeout(function(){
+            resolve("async hello");
+        }, 2000); // 2sec 
+    });
+}
+
+async function say_hello3(){
+    return new Promise((resolve, reject) => {
+        setTimeout(function(){
+            reject("async hello");
+        }, 2000); // 2sec 
+    });
+}
+
+wish = say_hello3(); 
+console.log(wish); 
+// it will give an error as its rejected 
+// to handle this we do the following 
+
+say_hello3()
+.then((v) => console.log(v))
+.catch(e => {
+    console.log("Got error"); 
+    console.log(e);
+})
+
+async function greetings(){
+    console.log("before"); 
+    wish = await say_hello1(); 
+    // this will give an error as await can only be used 
+    // in async function to make this work we have to make it a async function
+    console.log(wish); 
+    console.log("after");
+    return wish;
+}
+
+greetings();
+
+async function greetings2(){
+    console.log("before"); 
+    try{
+        wish = await say_hello3()
+        console.log(wish)
+        console.log(after)
+        return wish
+    }catch(e){
+        console.log("got error")
+        console.log(e)
+        return null
+    }
+}
+
+x = greetings2();
+
+// or we can do the following 
+x.then((v) => console.log(v))
+.catch(e => {
+    console.log("got error")
+    console.log(e)
+})
